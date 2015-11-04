8d17ff7707e0f757890390b70fb712db2d13e2f7
ryankynor <ryankynor@hanovernorwichschools.org>
2015-11-04 13:30:16 -0500

    Updated from Brython Server: 11/4/2015 1:30:16 PM

--- cryptography.py
+++ cryptography.py
@@ -13,12 +13,16 @@ associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .
 
 command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
- = [('a',1),('b',2),('c',3),('d',4),('e',5),('f',6),('g',7),('h',8),('i',9)
+tuples = [('a',0),('b',1),('c',2),('d',3),('e',4),('f',5),('g',6),('h',7),('i',8),("j",9),("k",10),("l",11),("m",12),("n",13),("o",14),("p",15),("q",16),("r",17),("s",18),("t",19),("u",20),("v",21),("w",22),("x",23),("y",24),("z",25)('A',26),("B",27),('C',28),('D',29),('E',30),('F',31),('g',6),('h',7),('i',8),("j",9),("k",10),("l",11),("m",12),("n",13),("o",14),("p",15),("q",16),("r",17),("s",18),("t",19),("u",20),("v",21),("w",22),("x",23),("y",24),("z",25
 """
 associations.find(char)
 associations[index]
 """
+while command <> "q":
+    for x in alphabet:
+        if x == str(command)
 
+"""
 if command == "e":
     message = input("Message: ")
     key = input("Key: ")
@@ -36,7 +40,7 @@ elif command == "q":
 
 else:
     print ("Did not understand command, try again.")
-    
+""
     
     
     
