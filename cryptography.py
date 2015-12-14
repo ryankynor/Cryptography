6fe7502d3c55883035c789fd55ee8669f0523086
ryankynor <ryankynor@hanovernorwichschools.org>
2015-12-14 14:53:23 -0500

    Updated from Brython Server: 12/14/2015 2:53:24 PM

--- cryptography.py
+++ cryptography.py
@@ -21,7 +21,7 @@ L1 = []
 L2 = []
 t = ""
 
-while t != "q":
+while command != "q":
     find = 1000000*associations
     if command == "q":
         print ("goodbye")
@@ -29,9 +29,9 @@ while t != "q":
         message1 =  input("message: ")
         key = input("key: ")
         for t in message1:
-            l1.append(associations.find (x))
+            l1.append(associations.find (t))
         for t in key:
-            l2.append(associations.find (x))
+            l2.append(associations.find (t))
         if len(l1) > len(l2):
             tvar = len(l1)/len(l2)
             c = math.ceil(tvar)
@@ -39,15 +39,15 @@ while t != "q":
             l2a = zip(l1a,l2)
             encryption = [t+x for t, x in l2a]
         for x in encryption:
-            print (fin[t],end="")
+            print (x,end="")
         print ("done")
     elif command == "d":
         message2 = input("Message: ")
         key2 = input("Key: ")
         for g in message2:
-            L1.append(association.find(g))
+            L1.append(associations.find(g))
         for g in key2:
-            L2.append(association.find(g))
+            L2.append(associations.find(g))
         if len(L1) > len(L2):
             tvar2 = float (len(L1)/len(L2))
             c = math.ceil(tvar2)
@@ -63,6 +63,8 @@ while t != "q":
         print ("done")
     else:
         print ("Did not understand command, please try again.")
+else:
+    print ("goodbye")
             
             
             
